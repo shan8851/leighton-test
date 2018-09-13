082aac6e38e48b9f0b13517afa4c962f1504db97
shan5742 <asamshan456@gmail.com>
2018-09-13 21:09:57 +0100

    finalising search component

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './Search.css';
+import 'tachyons'
 
 
 class Search extends Component {
     render() {
         return (
-            <div className="search">
-            <input type="text" name="search" placeholder="Search.."/>
+            <div class="dt w-100 mt4 mb2">
+                <div className="dtc w-25 search">
+                    <input type="text" name="search" placeholder="Search.."/>
+                 </div>
+                 <div class="dtc w-75">
+                    <a class="f6 grow no-underline br-pill ba ph3 pv2 mb2 mr4 dib dark-gray fr mt3" href="#0">£ Hight to Low</a>
+                    <a class="f6 grow no-underline br-pill ba ph3 pv2 mb2 mr4 dib dark-gray fr mt3" href="#0">£ Low to High</a>
+                 </div>
+                 
+
          </div>
         );
     }
