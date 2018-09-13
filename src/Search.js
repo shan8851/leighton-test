49799f323f9ed0ac5cf03614b6069caa65ac31e1
shan5742 <asamshan456@gmail.com>
2018-09-13 14:26:38 +0100

    more work with search component

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Search.css';
 
+
 class Search extends Component {
     render() {
         return (
-           <div>
-               SEARCH
-           </div>
+            <div className="search">
+            <input type="text" name="search" placeholder="Search.."/>
+         </div>
         );
     }
 }
