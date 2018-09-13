812bbc6369ab06d27696a526d07c10f7d048c8a6
shan5742 <asamshan456@gmail.com>
2018-09-13 11:36:29 +0100

    adding nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './Nav.css';
 import 'tachyons';
 
 class Nav extends Component {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Nav from './Nav';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
 ReactDOM.render(<Nav />, document.getElementById('root'));
 registerServiceWorker();
