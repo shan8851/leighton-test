a276b9204761509ef5dad33f3a52782cc7fc19d6
shan5742 <asamshan456@gmail.com>
2018-09-13 13:04:35 +0100

    adding language select to header

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import 'tachyons';
-import ReactCountryFlag from "react-country-flag";
+
 
 
 class Header extends Component {
     render() {
         return (
-            <div class="hide-child absolute ba ma4">
-                <ReactCountryFlag code="uk" svg />
-    <div class="child absolute ba">
-        <div> de </div>
-        <div > it </div>
-        <div > es </div>
-    </div>
-  </div>
+            <div class="fl w-10 ma4">
+                 <div class="hide-child absolute">
+                 <a class="f6 link dim ba ph3 pv2 mb2 dib near-black" href="#0">GB</a>
+                     <div class="child absolute ba">
+                            <div> <a class="f6 link dim ba ph3 pv2 mb2 dib near-black" href="#0">FR</a></div>
+                            <div> <a class="f6 link dim ba ph3 pv2 mb2 dib near-black" href="#0">DE</a></div>
+                            <div> <a class="f6 link dim ba ph3 pv2 mb2 dib near-black" href="#0">IT</a></div>
+                            <div> <a class="f6 link dim ba ph3 pv2 mb2 dib near-black" href="#0">ES</a></div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
-
 export default Header;
