b6521059d20d132b8ef20e8a81029ab06bc080a7
shan5742 <asamshan456@gmail.com>
2018-09-16 22:44:06 +0100

    adjusting map function

--- src/Product.js
+++ src/Product.js
@@ -46,72 +46,74 @@ class Product extends Component {
     } else {
       return (
         <div className="pb4">
-          <div className="flexContainer">
-            <div className="flexItem1">
-              <a href="https://placeholder.com">
-                <img src="https://via.placeholder.com/250x350" alt="" />
-              </a>
-            </div>
-            <div className="flexItem2">
-              <div className="productHead">
-                {products.map(Item => (
+          {products.map(Item => (
+            <div className="flexContainer">
+              <div className="flexItem1">
+                <a href="https://placeholder.com">
+                  <img src="https://via.placeholder.com/250x350" alt="" />
+                </a>
+              </div>
+              <div className="flexItem2">
+                <div className="productHead">
                   <h1 key={Item.productid} className="productTitle">
                     {Item.name}
                   </h1>
-                ))}
-                <h1 className="productPrice">£65</h1>
-                <div className="productMid">
-                  <h4>Brand:</h4>
-                  <h4>Colour:</h4>
-                  <h4>Product ID:</h4>
-                  <div className="f5 b">
-                    Size:
-                    <a
-                      className="f6 link dim ph3 pv2 ml3 mb2 dib white bg-light-red"
-                      href="#0"
-                    >
-                      S
-                    </a>
-                    <a
-                      className="f6 link dim ph3 pv2 ml2 mb2 dib white bg-light-red"
-                      href="#0"
-                    >
-                      M
-                    </a>
-                    <a
-                      className="f6 link dim ph3 pv2 ml2 mb2 dib white bg-light-red"
-                      href="#0"
-                    >
-                      L
-                    </a>
-                  </div>
-                  <h5>Description:</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Nunc sed velit dignissim sodales ut eu. Vitae purus
-                    faucibus ornare suspendisse sed nisi. Vel eros donec ac odio
-                    tempor. Ultrices neque ornare aenean euismod elementum nisi
-                    quis eleifend. Tellus integer feugiat scelerisque varius.{" "}
-                  </p>
-                  <div className="ProductFoot">
-                    <a
-                      className="f6 link dim ph3 pv2 mb2 dib white bg-light-red"
-                      href="#0"
-                    >
-                      Add to basket
-                    </a>
-                    <a
-                      className="f6 link dim ba ph3 pv2 mb2 ml4 dib black"
-                      href="#0"
-                    >
-                      Save for later
-                    </a>
+                  <h1 className="productPrice">£65</h1>
+                  <div className="productMid">
+                    <h4>Brand:</h4>
+                    <h4>Colour:</h4>
+                    <h4>Product ID:</h4>
+                    <div className="f5 b">
+                      Size:
+                      <a
+                        className="f6 link dim ph3 pv2 ml3 mb2 dib white bg-light-red"
+                        href="#0"
+                      >
+                        S
+                      </a>
+                      <a
+                        className="f6 link dim ph3 pv2 ml2 mb2 dib white bg-light-red"
+                        href="#0"
+                      >
+                        M
+                      </a>
+                      <a
+                        className="f6 link dim ph3 pv2 ml2 mb2 dib white bg-light-red"
+                        href="#0"
+                      >
+                        L
+                      </a>
+                    </div>
+                    <h5>Description:</h5>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Nunc sed velit dignissim sodales ut eu. Vitae
+                      purus faucibus ornare suspendisse sed nisi. Vel eros donec
+                      ac odio tempor. Ultrices neque ornare aenean euismod
+                      elementum nisi quis eleifend. Tellus integer feugiat
+                      scelerisque varius.{" "}
+                    </p>
+                    <div className="ProductFoot">
+                      <a
+                        className="f6 link dim ph3 pv2 mb2 dib white bg-light-red"
+                        href="#0"
+                      >
+                        Add to basket
+                      </a>
+                      <a
+                        className="f6 link dim ba ph3 pv2 mb2 ml4 dib black"
+                        href="#0"
+                      >
+                        Save for later
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
+          ;
         </div>
       );
     }
